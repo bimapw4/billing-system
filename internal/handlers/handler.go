4fd867f78398cc7897d993e0271df802019ff930
bimapw4 <bimapw4@gmail.com>
2025-07-27 16:46:58 +0700

    [loans] handler - add handler loans create and list

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -3,14 +3,17 @@ package handlers
 import (
 	"billing/internal/business"
 	"billing/internal/handlers/borrowers"
+	"billing/internal/handlers/loan"
 )
 
 type Handlers struct {
 	Borrowers borrowers.Handler
+	Loan      loan.Handler
 }
 
 func NewHandler(business business.Business) Handlers {
 	return Handlers{
-		Borrowers: borrowers.NewAdminHandler(business),
+		Borrowers: borrowers.NewBorrowerHandler(business),
+		Loan:      loan.NewLoanHandler(business),
 	}
 }
