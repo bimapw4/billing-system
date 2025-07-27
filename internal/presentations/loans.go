468fa5db2ad8b623afe62d44648764424a9bc267
bimapw4 <bimapw4@gmail.com>
2025-07-27 16:44:18 +0700

    [loans] presentations - add loans presentations

---
+++ internal/presentations/loans.go
@@ -0,0 +1,22 @@
+package presentations
+
+import (
+	"billing/internal/common"
+	"time"
+)
+
+const (
+	ErrLoanNotFound     = common.Error("err loan not found")
+	ErrLoanAlreadyExist = common.Error("err loan already exist")
+)
+
+type Loan struct {
+	ID           string    `db:"id" json:"id"`
+	BorrowerID   string    `db:"borrower_id" json:"borrower_id"`
+	Principal    int       `db:"principal" json:"principal"`
+	InterestRate int       `db:"interest_rate" json:"interest_rate"`
+	TotalWeeks   int       `db:"total_weeks" json:"total_weeks"`
+	StartDate    time.Time `db:"start_date" json:"start_date"`
+	CreatedAt    time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt    time.Time `db:"updated_at" json:"updated_at"`
+}
