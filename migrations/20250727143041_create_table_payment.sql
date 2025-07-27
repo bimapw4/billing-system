272ce54d380ddfd44ca0768bd7fa070b3223197d
bimapw4 <bimapw4@gmail.com>
2025-07-27 16:43:18 +0700

    [payment] migrations - update migrations payment

--- migrations/20250727143041_create_table_payment.sql
+++ migrations/20250727143041_create_table_payment.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS payments (
-    id          UUID        PRIMARY KEY,
-    loan_id     uuid        NOT NULL REFERENCES loans(id) ON DELETE CASCADE,
-    week        INT         NOT NULL CHECK (week BETWEEN 1 AND 50),
-    paid        BOOLEAN     NOT NULL DEFAULT FALSE,
+    id          UUID            PRIMARY KEY,
+    loan_id     uuid            NOT NULL REFERENCES loans(id) ON DELETE CASCADE,
+    week        INT             NOT NULL,
+    paid        INT             NOT NULL DEFAULT 0,
+    is_paid     BOOLEAN         NOT NULL DEFAULT FALSE,
     paid_at     TIMESTAMP,
     created_at  TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP
