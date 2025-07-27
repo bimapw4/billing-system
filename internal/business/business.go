36dfae5dbf073719b8104caf439fb04db4f3a217
bimapw4 <bimapw4@gmail.com>
2025-07-27 16:46:01 +0700

    [loan] business - add business loan create and list

--- internal/business/business.go
+++ internal/business/business.go
@@ -2,15 +2,18 @@ package business
 
 import (
 	"billing/internal/business/borrowers"
+	"billing/internal/business/loan"
 	"billing/internal/repositories"
 )
 
 type Business struct {
 	Borrowers borrowers.Borrower
+	Loan      loan.Loan
 }
 
 func NewBusiness(repo *repositories.Repository) Business {
 	return Business{
 		Borrowers: borrowers.NewBusiness(repo),
+		Loan:      loan.NewBusiness(repo),
 	}
 }
---
+++ internal/business/loan/loan.go
@@ -0,0 +1,58 @@
+package loan
+
+import (
+	"billing/internal/entity"
+	"billing/internal/presentations"
+	"billing/internal/repositories"
+	"billing/pkg/meta"
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Loan interface {
+	Create(ctx context.Context, payload *entity.Loan) (*presentations.Loan, error)
+	List(ctx context.Context, m *meta.Params) ([]presentations.Loan, error)
+}
+
+type business struct {
+	repo *repositories.Repository
+}
+
+func NewBusiness(repo *repositories.Repository) Loan {
+	return &business{
+		repo: repo,
+	}
+}
+
+func (b *business) Create(ctx context.Context, payload *entity.Loan) (*presentations.Loan, error) {
+
+	data := presentations.Loan{
+		ID:           uuid.NewString(),
+		BorrowerID:   payload.BorrowerID,
+		Principal:    payload.Principal,
+		InterestRate: payload.InterestRate,
+		TotalWeeks:   payload.TotalWeeks,
+		StartDate:    payload.StartDate,
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+	}
+
+	err := b.repo.Loans.Create(ctx, &data)
+	if err != nil {
+		fmt.Println("errr ==", err)
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+func (b *business) List(ctx context.Context, m *meta.Params) ([]presentations.Loan, error) {
+	list, err := b.repo.Loans.List(ctx, m)
+	if err != nil {
+		return nil, err
+	}
+	return list, nil
+}
